fe6ccee8963f4bf301f4deaecfe98b69d8fbef76
Tim Hyson <tim.hyson@wonga.com>
2016-03-18 15:44:20 +0000

    Inject service into controller.

--- app.js
+++ app.js
@@ -9,16 +9,11 @@ app.factory('posts', [function() {
 
 app.controller ('MainCtrl', [
   '$scope',
-  function ($scope) {
+  'posts',
+  function ($scope, posts) {
     $scope.test = 'Hello world';
 
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ];
+    $scope.posts = posts.posts;
 
     $scope.addPost = function () {
       if (!$scope.title || $scope.title === '') { return; }
@@ -30,7 +25,7 @@ app.controller ('MainCtrl', [
       $scope.title = '';
       $scope.link = '';
     };
-    
+
     $scope.incrementUpvotes = function(post) {
       post.upvotes += 1;
     };
